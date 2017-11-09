92b470a49ccc068d0968b190dc6f857cb23feb77
andresmechali <andresmechali@gmail.com>
2017-11-09 11:21:20 +0100

    graphQL support added

--- src/index.js
+++ src/index.js
@@ -4,16 +4,30 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './store';
 
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cj9sasgcc8ptt0101i0r37dgq' });
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <App />
-        </ConnectedRouter>
-    </Provider>,
+    <ApolloProvider client={client}>
+        <Provider store={store}>
+            <ConnectedRouter history={history}>
+                <App />
+            </ConnectedRouter>
+        </Provider>
+    </ApolloProvider>,
     document.getElementById('root')
 );
 
