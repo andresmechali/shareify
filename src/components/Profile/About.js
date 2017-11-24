ab01d6a0f732460813afca099c945e13cf6cb7db
andresmechali <andresmechali@gmail.com>
2017-11-24 18:33:41 +0100

    Make difference when profile completed 100%

--- src/components/Profile/About.js
+++ src/components/Profile/About.js
@@ -92,9 +92,11 @@ const About = (props) => {
                                 </span>
                             </span>
                         </div>
-                        <Link to='/profile/settings' className="btn btn-lg-2 btn-border-think custom-color c-grey full-width">
-                            Complete it now
-                        </Link>
+                        {percentageCompleted(props.user) === 100?
+                            <Link to='/profile/settings' className="btn btn-lg-2 btn-border-think custom-color c-grey full-width">
+                                Complete it now
+                            </Link>:''
+                        }
                     </div>
 
                 </ul>
