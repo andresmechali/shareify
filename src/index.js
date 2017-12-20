b001bf33ef366481e58040671a514d6711026c9d
andresmechali <andresmechali@gmail.com>
2017-12-20 15:40:29 +0100

    change link for deploying

--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ import store, { history } from './store';
 import { setCurrentUser, removeCurentUser } from "./redux/actions/authActions";
 import jwt from 'jsonwebtoken';
 
-const httpLink = new HttpLink({ uri: 'http://localhost:3001/graphql' });
-//const httpLink = new HttpLink({ uri: 'https://cool-server.herokuapp.com/graphql' });
+//const httpLink = new HttpLink({ uri: 'http://localhost:3001/graphql' });
+const httpLink = new HttpLink({ uri: 'https://cool-server.herokuapp.com/graphql' });
 
 const authMiddleware = new ApolloLink((operation, forward) => {
     operation.setContext({
